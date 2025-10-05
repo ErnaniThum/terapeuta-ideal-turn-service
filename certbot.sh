aa943606a94647b6787334e4b7937c990be488ea
Ernani Thum <ernani.thum@gmail.com>
2025-10-05 10:01:51 -0300

    Initial commit

--- certbot.sh
+++ certbot.sh
@@ -1,22 +1,26 @@
+#!/bin/bash
+set -e
 
-echo "Starting nginx and certbot containers..."
-
-NGINX_PID= $(docker run -d \
+echo "Starting nginx container..."
+NGINX_PID=$(docker run -d \
   --name nginx \
   -p 80:80 \
   -p 443:443 \
-  -v ./default.conf:/etc/nginx/conf.d/default.conf \
-  -v ./letsencrypt:/etc/letsencrypt \
-  -v ./certbot:/var/www/certbot \
+  -v "$(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro" \
+  -v "$(pwd)/letsencrypt:/etc/letsencrypt" \
+  -v "$(pwd)/certbot:/var/www/certbot" \
   nginx:latest)
 
 echo "Obtaining SSL certificates with Certbot..."
 docker run -it --rm --name certbot \
-            -v "/etc/letsencrypt:/etc/letsencrypt" \
-            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-            certbot/certbot certonly
-
+  -v "$(pwd)/letsencrypt:/etc/letsencrypt" \
+  -v "$(pwd)/certbot:/var/www/certbot" \
+  certbot/certbot certonly --webroot \
+  --webroot-path /var/www/certbot \
+  -d your-domain.com -d www.your-domain.com
 
 echo "Stopping nginx container..."
-docker stop $NGINX_PID
-docker rm $NGINX_PID
+docker stop "$NGINX_PID"
+docker rm "$NGINX_PID"
+
+echo "Done! Certificates saved in ./letsencrypt"
